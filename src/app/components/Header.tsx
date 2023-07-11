a1cb265cde345339542b8a13d1a5fd7e018166b2
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-07-11 23:23:09 +0200

    Update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = () => (<header className='px-80 pt-10 flex justify-between'>
 <div>
-  <Link href="/">ChrisClaude.</Link>
+  <Link href="/" className='flex items-center'>
+  <Image
+      src="/ChrisClaudeLogo_.jpeg"
+      width={90}
+      height={90}
+      alt="Chris Claude Logo"
+      className='mr-2'
+    />
+    <span>Chris Claude</span>
+  </Link>
 </div>
-<nav>
+<nav className='flex items-center'>
   <ul className='flex'>
     <li className='mr-9'>
       <Link href="/">Home</Link>
