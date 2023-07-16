cd8969fff2cbfc164704097fbcedb967c29ec7b3
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-07-16 02:07:51 +0200

    Update Main.tsx

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 const Main = ({children}: {
   children: ReactNode
 }) => (
-  <main className="md:px-52 py-6 px-80">{children}</main>
+  <main className="md:px-52 md:py-32 py-12 px-80">{children}</main>
 );
 
 export default Main;
