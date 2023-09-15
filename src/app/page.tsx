1fe424ed1be86bad756b9cf51a3be219ea57ef75
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-09-15 21:59:52 +0200

    Add Dev Weekly section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { SiCsharp, SiMicrosoftazure, SiMicrosoftsqlserver, SiDotnet } from 'react-icons/si';
+import { BsArrowRightShort } from 'react-icons/bs';
 import { FaReact } from 'react-icons/fa';
 import { TbBrandNextjs } from 'react-icons/tb';
 
@@ -11,7 +12,7 @@ export default function Home() {
         <p className='text-xl font-semibold mb-3'>Chris Claude</p>
         <h1 className='text-7xl font-semibold text-center leading-tight mb-7'>Learn & Improve Your <br/>Software Development Skills</h1>
         <p className='text-2xl max-w-xl mb-10 mx-auto text-center'>
-        Full-stack project based tutorials for beginners and intermediate, and advanced developers.
+        Full-stack project based tutorials for beginners, intermediate, and advanced developers.
         </p>
         <div className='flex justify-center sm:mb-12 mb-10 md:mb-16 lg:mb-20 xl:mb-24'>
           <Link href="/courses" className='bloc bg-white text-blue-600 px-6 py-4 rounded-lg text-lg font-semibold hover:bg-gray-200 focus:ring'>Get started</Link>
@@ -30,69 +31,65 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className='flex justify-between items-center py-8'>
-        <p className='text-lg uppercase'>
-          Notify me when new courses are available
-        </p>
-        <div>
-          <input type="email" placeholder='Email' className='mr-6 outline outline-1 outline-gray-400 rounded p-1 w-60 focus:outline-blue-600' />
-          <button className='bg-white text-black px-3 py-2 rounded text-sm font-semibold focus:ring hover:bg-gray-200' type='submit'>Notify me</button>
-        </div>
-      </section>
-      <section>
-        <h1>Latest Courses & Posts</h1>
-        <div>
-          {/* TODO: This should come from a directory with files (.md files) that we can parse and loop through */}
+      <div className='relative'>
+        <div className='absolute bg-grid inset-0'/>
+        <section>
+          <div className='flex items-center flex-col mb-14'>
+            <span className='block uppercase mb-3'>Recent Posts Of</span>
+            <h1 className='text-5xl'>The <span className='text-blue-500'>Dev</span> Weekly</h1>
+          </div>
+          <div className='flex justify-center grid-cols-1 gap-6 mb-36 max-w-screen-xl mx-auto sm:grid-cols-2 lg:grid-cols-3'>
+            {/* TODO: This should come from a directory with files (.md files) that we can parse and loop through */}
+            <article>
+              <div  className='card mb-3'>
+                <h2>Build and Secure an API with ASP.NET and Identity Server</h2>
+              </div>
+              <div>
+                <div className='flex justify-between items-center'>
+                  <h2>We leverage the OAuth2 and OpenId protocols to implement authentication</h2>
+                  <BsArrowRightShort className='text-4xl' />
+                </div>
+              </div>
+            </article>
+            <article>
+              <div className='card mb-2'>
+                <h2>How to Handle Exceptions in ASP.NET</h2>
+              </div>
+              <div>
+                <div className='flex justify-between items-center'>
+                  <h2>We leverage the OAuth2 and OpenId protocols to implement authentication</h2>
+                  <BsArrowRightShort className='text-4xl' />
+                </div>
+              </div>
+            </article>
+
+            <article>
+              <div className='card'>
+                <h2>Build a full-stack web app with ASP.NET and React JS</h2>
+              </div>
+              <div>
+                <div className='flex justify-between items-center'>
+                  <h2>We leverage the OAuth2 and OpenId protocols to implement authentication</h2>
+                  <BsArrowRightShort className='text-4xl' />
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
+
+        <section>
           <div>
-            <h2>Build and Secure an API with ASP.NET and Identity Server</h2>
-            <h3>Intermediate</h3>
-            <div>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt maxime voluptates sapiente suscipit, nesciunt vero.</div>
-            <button>
-              Watch
-            </button>
-            <button>
-              Read
-            </button>
+            Image
           </div>
           <div>
-            <h2>Build and Secure an API with ASP.NET and Identity Server</h2>
-            <h3>Intermediate</h3>
-            <div>description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, nam repellendus asperiores reprehenderit quibusdam vero!</div>
+            <h1>Chris Claude on YouTube</h1>
+            <p>Subscribe to my YouTube channel for more tutorials</p>
             <button>
-              Watch
-            </button>
-            <button>
-              Read
+              View channel
             </button>
           </div>
-          <div>
-            <h2>Build an Azure Boards Clone with ASP.NET & Next JS</h2>
-            <h3>Intermediate</h3>
-            <div>description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi minus odit soluta ea temporibus praesentium.</div>
-            <button>
-              Watch
-            </button>
-            <button>
-              Read
-            </button>
-          </div>
-          <button>
-            Show all courses
-          </button>
-        </div>
-      </section>
-      <section>
-        <div>
-          Image
-        </div>
-        <div>
-          <h1>Chris Claude on YouTube</h1>
-          <p>Subscribe to my YouTube channel for more tutorials</p>
-          <button>
-            View channel
-          </button>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
