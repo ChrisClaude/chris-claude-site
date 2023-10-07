c33a29e2dc8157b396dc5980bb8a19edaf9b148e
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-10-07 17:18:17 +0200

    Update Pagination.tsx

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -19,7 +19,7 @@ const Pagination = ({ currentPage, numPages }: { currentPage: number, numPages: 
           </Link>
         )}
         {Array.from({ length: numPages }, (_, i) => (
-          <Link href={`/blog?page=${i + 1}`} key={`page-${i}`}>
+          <Link href={`/blog?page=${i + 1}#articles`} key={`page-${i}`}>
             <li className='relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-gray-800 mr-1 hover:bg-gray-200 cursor-pointer rounded-lg'>
               {i + 1}
             </li>
