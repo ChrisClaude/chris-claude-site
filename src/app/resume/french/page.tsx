07b99705d6300240e6f81955c115ae8526cc3d30
Chris Claude <chris.de-tchambila@cleverbit.software>
2024-09-07 21:24:12 +0200

    Add french cv

--- src/app/resume/french/page.tsx
+++ src/app/resume/french/page.tsx
@@ -16,7 +16,7 @@ import { MdLocationPin, MdOutlineAlternateEmail } from 'react-icons/md';
 const Resume = () => {
   const { uiState, setUIState } = useContext(UIContext);
   const technologies = [
-   'C#',
+    'C#',
     'C++',
     'JavaScript',
     'React JS',
@@ -50,19 +50,19 @@ const Resume = () => {
   }[] = [
     {
       fullName: 'Prajwal Chengappa',
-      role: 'Senior Software Engineer',
+      role: 'Ingénieur en génie logiciel Senior',
       email: 'prajwal.chengappa@capgemini.com',
       company: 'Capgemini',
     },
     {
       fullName: 'Banish Jha',
-      role: 'Senior Software Engineer',
+      role: 'Ingénieur en génie logiciel Senior',
       email: 'banish.jha@nn-group.com',
       company: 'Nationale Nederlanden',
     },
     {
       fullName: 'Rens Van Driel',
-      role: 'Software Developer',
+      role: 'Ingénieur en génie logiciel',
       email: 'rens.van.a.driel@sogeti.com',
       company: 'Sogeti',
     },
@@ -74,9 +74,7 @@ const Resume = () => {
 
   return (
     <div className="bg-white text-gray-800 py-24 px-52 overflow-x-auto xl:flex xl:justify-center">
-      <div
-        className="flex flex-col justify-center"
-        style={{ width: '1200px' }}>
+      <div className="flex flex-col justify-center" style={{ width: '1200px' }}>
         <div>
           {/* Header */}
           <div className="flex justify-between mb-10">
@@ -86,7 +84,7 @@ const Resume = () => {
                   Claude De-Tchambila
                 </h1>
                 <h2 className="text-2xl font-semibold text-blue-500">
-                  Software Engineer
+                  Ingénieur en génie logiciel
                 </h2>
               </div>
               <div className="grid grid-cols-2">
@@ -109,7 +107,7 @@ const Resume = () => {
                 </a>
                 <p className="flex items-center">
                   <MdLocationPin className="mr-1 text-blue-500" />
-                  Poznan, Poland
+                  Poznan, Pologne
                 </p>
               </div>
             </div>
@@ -131,17 +129,18 @@ const Resume = () => {
             <div>
               <div className="mb-6">
                 <div className="mb-3">
-                  <h2 className="text-xl font-semibold uppercase">Summary</h2>
+                  <h2 className="text-xl font-semibold uppercase">Profil</h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
                 <p>
-                  I am a software engineer that specializes in designing
-                  distributed systems and implementing software solutions. With
-                  a strong background in building full-stack web, cloud, and
-                  mobile applications, I have developed excellent
-                  problem-solving skills and a love for constant learning. I am
-                  a critical thinker with a passion for positively impacting
-                  people&apos;s lives.
+                  Je suis ingénieur en genie logiciel spécialisé dans la
+                  conception de systèmes distribués et la mise en œuvre de
+                  solutions logicielles. Fort d&apos;une solide expérience dans
+                  le développement d&apos;applications web, et cloud, j&apos;ai
+                  développé d&apos;excellentes compétences en programmation
+                  informatique ainsi qu&apos;une passion pour
+                  l&apos;apprentissage continu. Je suis un penseur critique avec
+                  la passion d&apos;avoir un impact positif sur la vie des gens.
                 </p>
               </div>
 
@@ -149,98 +148,112 @@ const Resume = () => {
               <div className="mb-6">
                 <div className="mb-3">
                   <h2 className="text-xl font-semibold uppercase">
-                    Corporate Experience
+                    Expérience professionnelle
                   </h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
                 <div className="mb-4 border-b-2 border-dashed border-gray-400 pb-4">
-                  <h3 className="text-lg font-medium">Software Engineer</h3>
+                  <h3 className="text-lg font-medium">
+                    Ingénieur en génie logiciel
+                  </h3>
                   <p className="text-blue-500">Capgemini</p>
                   <p className="flex items-center">
                     <span>07/2022</span>
                     <span className="mx-1"> - </span> <span>Present</span>{' '}
-                    <MdLocationPin className="ml-3 mr-1" /> Poznan, Poland
+                    <MdLocationPin className="ml-3 mr-1" /> Poznan, Pologne
                   </p>
                   <div className="mt-1">
                     <p>
-                      I have been working on architecting and implementing
-                      software solutions for clients in the financial services
-                      sector. The list of clients I have worked with includes
-                      (but is not limited to) <b>Nationale Nederlanden</b>, one
-                      of the biggest insurance providers in the Netherlands. I
-                      have designed and implemented features that have improved
-                      the business processes of my clients. The technologies I
-                      have used so far include C#, SQL Server, Microsoft Azure,
-                      React JS, TypeScript and AzureDevOps. The following are a
-                      few of the features I have worked on:
+                      J&apos;ai travaillé sur l&apos;architecture et la mise en œuvre de
+                      solutions logicielles pour des clients dans le secteur des
+                      services financiers. La liste des clients avec lesquels
+                      j&apos;ai collaboré inclut (sans s&apos;y limiter) Nationale
+                      Nederlanden, l&apos;un des plus grands fournisseurs
+                      d&apos;assurances aux Pays-Bas. J&apos;ai conçu et implémenté des
+                      fonctionnalités qui ont amélioré les processus métiers de
+                      mes clients. Les technologies que j&apos;ai utilisées jusqu&apos;à
+                      présent incluent C#, SQL Server, Microsoft Azure, React
+                      JS, TypeScript et AzureDevOps. Voici quelques-unes des
+                      fonctionnalités sur lesquelles j&apos;ai travaillé :
                     </p>
                     <ul className="list-disc ml-10 mt-1">
                       <li>
-                        Slowly changing dimension type 2 for a data warehouse
-                        system
+                        Dimension à changement lent de type 2 pour un système de
+                        data warehouse
                       </li>
                       <li>
-                        Cross environment and secure data import and export
+                        Importation et exportation de données sécurisées et
+                        inter-environnements
                       </li>
                       <li>
-                        Data row level security integrated with Azure RBAC
+                        Sécurité au niveau des lignes de données intégrée avec
+                        Azure RBAC
                       </li>
                     </ul>
                   </div>
                 </div>
                 <div className="mb-4 border-b-2 border-dashed border-gray-400 pb-4">
-                  <h3 className="text-lg font-medium">Software Engineer</h3>
+                  <h3 className="text-lg font-medium">
+                    Ingénieur en génie logiciel
+                  </h3>
                   <p className="text-blue-500">Dariel</p>
                   <p className="flex items-center">
                     <span>02/2021</span>
                     <span className="mx-1"> - </span> <span>06/2023</span>{' '}
-                    <MdLocationPin className="ml-3 mr-1" /> Johannesburg, South
-                    Africa
+                    <MdLocationPin className="ml-3 mr-1" /> Johannesburg,
+                    Afrique du Sud
                   </p>
                   <div className="mt-1">
                     <p>
-                      I have worked with a number of clients on multiple
-                      projects in the financial service sector. My clients
-                      included <b>Sasfin Bank</b> and <b>Safrican Insurance</b>,
-                      which is now part of <b>Sanlam</b>, the largest insurance
-                      provider in South Africa. The following are a few of the
-                      projects I have contributed to:
+                      J&apos;ai travaillé avec plusieurs clients sur divers
+                      projets dans le secteur des services financiers. Mes
+                      clients incluaient Sasfin Bank et Safrican Insurance,
+                      désormais partie de Sanlam, le plus grand fournisseur
+                      d&apos;assurances en Afrique du Sud. Voici quelques
+                      projets auxquels j&apos;ai contribué
                     </p>
                     <ul className="list-disc ml-10 mt-1">
                       <li>
-                        A Forex exchange trading platform. The technologies used
-                        were C#, TypeScript, ASP.NET Core, Angular, AzureDevOps,
-                        RabbitMQ
+                        Une plateforme de trading Forex. Les technologies
+                        utilisées étaient C#, TypeScript, ASP.NET Core, Angular,
+                        AzureDevOps, RabbitMQ
                       </li>
                       <li>
-                        Insurance sales system. The technologies used were C#,
-                        TypeScript, ASP.NET Core, Angular, React, AzureDevOps
+                        Un système de vente d&apos;assurances. Les technologies
+                        utilisées étaient C#, TypeScript, ASP.NET Core, Angular,
+                        React, AzureDevOps
                       </li>
                       <li>
-                        A weighbridge management software system in the logistic
-                        industry. The technologies used were C#, TypeScript,
-                        ASP.NET Core, Angular, Jira, Jenkins, RabbitMQ
+                        Un logiciel de gestion de pont-bascule dans
+                        l&apos;industrie logistique. Les technologies utilisées
+                        étaient C#, TypeScript, ASP.NET Core, Angular, Jira,
+                        Jenkins, RabbitMQ
                       </li>
                     </ul>
                   </div>
                 </div>
                 <div>
-                  <h3 className="text-lg font-medium">Software Engineer</h3>
+                  <h3 className="text-lg font-medium">
+                    Ingénieur en génie logiciel
+                  </h3>
                   <p className="text-blue-500">Exigent Group</p>
                   <p className="flex items-center">
                     <span>11/2020</span>
                     <span className="mx-1"> - </span> <span>02/2021</span>{' '}
-                    <MdLocationPin className="ml-3 mr-1" /> Cape Town, South
-                    Africa
+                    <MdLocationPin className="ml-3 mr-1" /> Cape Town, Afrique
+                    du Sud
                   </p>
                   <p className="mt-1">
-                    I mainly worked on a project that was aimed at creating a
-                    software system that would help process large amounts of
-                    documents (company contracts, bills, legal agreements) using
-                    machine learning and providing critical insights to clients.
-                    I worked on backend and frontend features and integration
-                    with ML tools. The technologies used were Python, Django,
-                    Keras, PyTorch, and React JS.
+                    J&apos;ai principalement travaillé sur un projet visant à
+                    créer un système logiciel permettant de traiter de grandes
+                    quantités de documents (contrats d&apos;entreprise,
+                    factures, accords juridiques) en utilisant
+                    l&apos;apprentissage automatique et en fournissant des
+                    informations critiques aux clients. J&apos;ai contribué au
+                    développement des fonctionnalités backend et frontend ainsi
+                    qu&apos;à l&apos;intégration avec des outils de machine
+                    learning. Les technologies utilisées étaient Python, Django,
+                    Keras, PyTorch et React JS.
                   </p>
                 </div>
               </div>
@@ -249,7 +262,7 @@ const Resume = () => {
               <div>
                 <div className="mb-3">
                   <h2 className="text-xl font-semibold uppercase">
-                    References
+                    Références
                   </h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
@@ -278,7 +291,7 @@ const Resume = () => {
             <div>
               <div className="mb-6">
                 <div className="mb-3">
-                  <h2 className="text-xl font-semibold uppercase">Toolbox</h2>
+                  <h2 className="text-xl font-semibold uppercase">Outils</h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
 
@@ -299,9 +312,9 @@ const Resume = () => {
                 </div>
 
                 {/* Power Skills  */}
-                <div>
+                {/* <div>
                   <h3 className="text-lg font-semibold uppercase mb-1 text-blue-500">
-                    Power Skills
+                    Compétences
                   </h3>
                   <div className="flex flex-wrap gap-1">
                     {skills.map((skill, index) => (
@@ -312,20 +325,18 @@ const Resume = () => {
                       </span>
                     ))}
                   </div>
-                </div>
+                </div> */}
               </div>
 
               {/* Education */}
               <div className="mb-6">
                 <div className="mb-3">
-                  <h2 className="text-xl font-semibold uppercase">Education</h2>
+                  <h2 className="text-xl font-semibold uppercase">Éducation</h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
                 <ul>
                   <li className="border-b-2 border-dashed border-gray-400 pb-4">
-                    <p className="text-lg">
-                      Diploma in Application Development
-                    </p>
+                    <p className="text-lg">Licence en génie logiciel</p>
                     <p className="text-blue-500">
                       <a href="https://www.cput.ac.za/" target="_blank">
                         Cape Peninsula University of Technology
@@ -334,11 +345,12 @@ const Resume = () => {
                     <p className="flex items-center mr-1">
                       <span>01/2018</span> <span className="mx-1">-</span>{' '}
                       <span className="mr-3">12/2020</span>{' '}
-                      <MdLocationPin className="mr-1" /> Cape Town, South Africa
+                      <MdLocationPin className="mr-1" /> Cape Town, Afrique du
+                      Sud
                     </p>
                   </li>
                   <li className="mt-2">
-                    <p className="text-lg">Chemical Engineering</p>
+                    <p className="text-lg">Ingénierie chimique</p>
                     <p className="text-blue-500">
                       <a
                         href="https://www.ctc.edu.za/cape-town-campus/"
@@ -348,7 +360,8 @@ const Resume = () => {
                     </p>
                     <p className="flex items-center mr-1">
                       <span className="mr-3">2017</span>
-                      <MdLocationPin className="mr-1" /> Cape Town, South Africa
+                      <MdLocationPin className="mr-1" /> Cape Town, Afrique du
+                      Sud
                     </p>
                   </li>
                 </ul>
@@ -357,14 +370,14 @@ const Resume = () => {
               {/* Languages  */}
               <div className="mb-6">
                 <div className="mb-3">
-                  <h2 className="text-xl font-semibold uppercase">Languages</h2>
+                  <h2 className="text-xl font-semibold uppercase">Langues</h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
                 <ul className="flex flex-col gap-y-2">
                   <li className="flex justify-between">
                     <div>
-                      <p className="font-medium">English</p>
-                      <p className="text-sm">Advanced</p>
+                      <p className="font-medium">Anglais</p>
+                      <p className="text-sm">Avancé</p>
                     </div>
                     <div className="flex  gap-x-1">
                       <div className="h-8 w-2 bg-blue-500 rounded-full" />
@@ -376,8 +389,8 @@ const Resume = () => {
                   </li>
                   <li className="flex justify-between">
                     <div>
-                      <p className="font-medium">French</p>
-                      <p className="text-sm">Advanced</p>
+                      <p className="font-medium">Français</p>
+                      <p className="text-sm">Avancé</p>
                     </div>
                     <div className="flex gap-x-1">
                       <div className="h-8 w-2 bg-blue-500 rounded-full" />
@@ -389,8 +402,8 @@ const Resume = () => {
                   </li>
                   <li className="flex justify-between">
                     <div>
-                      <p className="font-medium">German</p>
-                      <p className="text-sm">Beginner</p>
+                      <p className="font-medium">Allemand</p>
+                      <p className="text-sm">Débutant</p>
                     </div>
                     <div className="flex gap-x-1">
                       <div className="h-8 w-2 bg-blue-500 rounded-full" />
@@ -407,7 +420,7 @@ const Resume = () => {
               <div className="mb-6">
                 <div className="mb-3">
                   <h2 className="text-xl font-semibold uppercase">
-                    Find me online
+                    Me trouver en ligne
                   </h2>
                   <div className="bg-gray-800 w-full h-1 rounded-full"></div>
                 </div>
@@ -415,7 +428,7 @@ const Resume = () => {
                   <li className="border-b border-dashed border-gray-600 pb-2">
                     <p className="flex items-center gap-x-1  font-medium">
                       <GiEarthAfricaEurope className="text-xl text-blue-500" />
-                      Site
+                      Site web
                     </p>
                     <a
                       href="https://chrisclaude.com"
