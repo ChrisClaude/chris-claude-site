c72843ce04c46c885c753113745f2de8104f8962
Chris Claude <christ.tchambila@gmail.com>
2025-07-11 17:00:10 +0200

    Add EditArticlePage

---
+++ frontend/src/app/admin/articles/[slug]/edit/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// For Rich text user --> https://github.com/tinymce/tinymce?tab=readme-ov-file
+const EditArticlePage = () => {
+  return <div>EditArticlePage</div>;
+};
+
+export default EditArticlePage;
