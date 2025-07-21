16eff6df77bd9cc8eaf49ddcd619476f6e237216
Chris Claude <christ.tchambila@gmail.com>
2025-07-21 14:34:59 +0200

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="/cdn-cgi/zaraz/s.js"
+          referrerPolicy="origin"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body suppressHydrationWarning={true}>
         <UIContextProvider>
           <AppWrapper>
